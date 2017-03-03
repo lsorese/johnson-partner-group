4a3259450759f8d72c4ef5af65428e9971e97cd6
Logan Sorese <lsorese@Logans-MacBook-Pro.local>
2017-03-03 15:07:17 -0800

    Commit for first email to Brandon

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,67 +1,59 @@
-    jQuery(function() {
-        var swiper = new Swiper('.swiper-container', {
-            pagination: '.base__pagination .row',
-            paginationClickable: true,
-            slidesPerView: 1,
-            spaceBetween: 0,
-            keyboardControl: true,
-            mousewheelControl: true,
-            direction: 'vertical',
-            onSlideChangeEnd: function(swiper) {
-                $('.bullet').removeClass('active');
-                $('.bullet[data-index="' + swiper.activeIndex + '"]').addClass('active');
-            },
-            paginationBulletRender: function(swiper, index, className) {
-                return '<div class="bullet" data-name="" data-index="' + index + '"><span class="bullet__one"></span><span class="bullet__name"></span><span class="bullet__two" data-name="name"></span></div>';
+jQuery(function() {
+
+    jQuery('.swiper-slide:nth-child(1)').addClass('active');
+
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.base__pagination .row',
+        paginationClickable: true,
+        slidesPerView: 1,
+        spaceBetween: 0,
+        keyboardControl: true,
+        mousewheelControl: true,
+        direction: 'vertical',
+        onSlideChangeStart: function(swiper) {
+            if (swiper.activeIndex !== 0) {
+                $('.base__social').show();
+                $('.base__social').addClass('active');
+            } else {
+                $('.base__social').hide();
+                $('.base__social').removeClass('active');
+
             }
-        });
 
-        $('.home__scroll').on('click', function(e) {
-            swiper.slideTo(1, 1000);   
-        });
+        }
+    })
 
-        $(".home__h2__rewrite").typed({
-            strings: ["This is the first sentence.", "We do some stuff and some more stuff.", "Lastly, we do a lot of very long stuff. Very, very long."],
-            typeSpeed: 0
-        });
-
-$('.sub').mousemove(function(e){
-    var amountMovedX = (e.pageX * -1 / 6);
-    var amountMovedY = (e.pageY * -1 / 6);
-    $(this).css('background-position', amountMovedX + 'px ' + amountMovedY + 'px');
-});
-
-$('.sub__bg2').mousemove(function(e){
-    var amountMovedX = (e.pageX * -1 / 6);
-    var amountMovedY = (e.pageY * -1 / 6);
-    $(this).css('background-position', amountMovedX + 'px ' + amountMovedY + 'px');
-});
-
-
-
-        /*
-
-                $(".animsition").animsition({
-                    inClass: 'fade-in-left',
-                    outClass: 'fade-out-right',
-                    inDuration: 800,
-                    outDuration: 800,
-                    linkElement: '.animsition-link',
-                    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
-                    loading: true,
-                    loadingParentElement: 'body', //animsition wrapper element
-                    loadingClass: 'animsition-loading',
-                    loadingInner: '', // e.g '<img src="loading.svg" />'
-                    timeout: false,
-                    timeoutCountdown: 5000,
-                    onLoadEvent: true,
-                    browser: ['animation-duration', '-webkit-animation-duration'],
-                    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-                    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-                    overlay: false,
-                    overlayClass: 'animsition-overlay-slide',
-                    overlayParentElement: 'body',
-                    transition: function(url) { window.location.href = url; }
-                }); */
-
+    $('.home__scroll').on('click', function(e) {
+        swiper.slideTo(1, 1000);
     });
+
+    setTimeout(function() {
+        $(".home__h2__rewrite").typed({
+            strings: [
+                "Video Conferencing.",
+                "Cloud &amp; Web Solutions.",
+                "Network and Infrastructure.",
+                "Service Delivery.",
+                "Quality Assurance.",
+                "End User Experience.",
+                "Organizational Transformation.",
+                "Project Management.",
+                "Branding.",
+                "Marketing.",
+                "Web Development."
+            ],
+            typeSpeed: 30
+        });
+    }, 1000);
+
+    $(".cd").logosDistort({
+        effectWeight: 2,
+        outerBuffer: 1.08,
+        elementDepth: 200,
+        perspectiveMulti: 2.5,
+        enableSmoothing: true
+    });
+
+
+
+});
